a86adf6f730d6e5c6fa83a631ccd0058d440a482
unknown <peterbillgamboa@protonmail.com>
2026-07-13 00:41:19 +0800

    updated version

--- 02-arrays-and-objects/01-array-basics/script.js
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -1,6 +1,30 @@
 let x;
 
+// Array literal
+const numbers = [12, 45, 33, 29, 39];
+const mixed = [12, 'hello', true, null];
 
-x = 'tutubag';
+// Array constructor
+
+const fruits = new Array('apple', 'grape', 'orange');
+
+
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+x = `My Favorite fruits is ${fruits[2]}`;
+
+x = numbers.length;
+
+fruits[2] = 'pear';
+
+// fruits.length = 2;
+
+fruits[3] = 'straw';
+
+fruits[fruits.length] = 'blueberry';
+
+x = fruits;
 
 console.log(x);
---
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -0,0 +1,22 @@
+let x;
+
+const arr = [34, 55, 95, 20, 15];
+
+// add array on the end
+arr.push(100);
+
+// remove end of array
+arr.pop();
+
+// add array on the first
+arr.unshift(99);
+
+// remove the first array
+arr.shift();
+
+arr.reverse();
+
+
+
+
+console.log(arr);
